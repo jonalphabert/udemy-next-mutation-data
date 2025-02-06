964a3c8b1139aa2cc55e0e4db30307bec86cf476
Jonathan Alphabet Sutanto <jonathan.alphabet@jasuindoad.local>
2025-02-06 14:27:03 +0700

    173. Validate useFormState hook

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,15 +1,44 @@
+"use client"
+
 import { storePost } from '@/lib/posts';
 import {redirect} from "next/navigation";
 import FormSubmit from "@/components/FormSubmit";
+import {useFormState} from "react-dom";
 
 export default function NewPostPage() {
   async function createPost(formData) {
     "use server";
+
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
+    let errors = []
+
     console.log(title, image, content);
+
+    if(!title || title.trim().length === 0) {
+      errors.push({
+        label: "title",
+        message: 'Please enter a title'
+      });
+    }
+    if(!image) {
+      errors.push({
+        label: "image",
+        message: 'Please upload an image'
+      });
+    }
+    if(!content || content.trim().length === 0) {
+      errors.push({
+        label: "content",
+        message: 'Please enter a content field'
+      });
+    }
+
+    if(errors.length > 0) {
+      return {errors}
+    }
 
     await storePost({
       imageUrl: '',
@@ -21,13 +50,16 @@ export default function NewPostPage() {
     redirect("/feed")
   }
 
+  const [state, formAction] = useFormState(createPost, {});
+
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input type="text" id="title" name="title" required/>
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
@@ -36,11 +68,12 @@ export default function NewPostPage() {
             accept="image/png, image/jpeg"
             id="image"
             name="image"
+            required
           />
         </p>
         <p className="form-control">
           <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
+          <textarea id="content" name="content" rows="5" required/>
         </p>
         <p className="form-actions">
           <FormSubmit />
